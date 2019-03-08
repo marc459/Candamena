aeee4b2da3edb0f8fc72d9255c34a059f78831c3
Marcos <santosserranomarcos@gmail.com>
2019-03-08 14:35:45 +0100

    Commit_2

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,9 +1,13 @@
 window.onload = CrearPaneles;
 function CrearPaneles(){
-    for(var i=1;i<5;i++){
+    for(var i=1;i<7;i++){
         var midiv = document.createElement("div");
         midiv.setAttribute("id","panel"+i);
         midiv.setAttribute("class","panel");
         document.getElementById('cuerpo').appendChild(midiv);
+
+        /*var botones = document.createElement("button");
+        botones.setAttribute("class","botones");
+        document.getElementsByClassName('panel').appendChild(botones);*/
     }
 }
